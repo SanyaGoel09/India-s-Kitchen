a8746ee40567f05939895d597315a756619d5b04
SanyaGoel09 <144154166+SanyaGoel09@users.noreply.github.com>
2024-04-21 13:07:23 +0530

    Add files via upload

---
+++ admin_page.php
@@ -0,0 +1,23 @@
+<?php
+    @include 'config.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>user page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <h3>Hi, <span>Admin</span></h3>
+            <h1>Welcome</h1>
+            <p>We will visit at your address shortly for the verification checks.</p>
+            <a href="logout.php" class="btn">logout</a>
+        </div>
+    </div>
+</body>
+</html>
---
+++ config.php
@@ -0,0 +1,9 @@
+<?php 
+    $conn = mysqli_connect('localhost:3307','root','','user_db');
+
+    if (!$conn) {
+        echo " did not connected";
+    }
+    
+
+?>
