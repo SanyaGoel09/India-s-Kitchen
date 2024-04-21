1455330b1178b54c073686109a1b3c2ba230cb8d
Bhavya Chawla <145850764+Bhavya-Chawla@users.noreply.github.com>
2024-04-21 13:04:33 +0530

    Add files via upload
    Commit by Bhavya

---
+++ main.php
@@ -0,0 +1,159 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--style-->
+    <link rel="stylesheet" href="stylenew.css">
+    <!--font: Notosans -->
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP& display=swap" rel="stylesheet">
+    <!-- icons: font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <style>
+        .dashboard-order{
+            background-image: url('promo.png');
+            background-size: cover;
+            background-position: center;
+            font-family: Arial, sans-serif;
+        }
+        </style>
+    <title>Responsive Food Delivery Dashboard</title>
+</head>
+<body>
+    <!-- header -->
+    <input type="checkbox" id="cart">
+    <label for="cart" class="label-cart"><span class="fas fa-shopping-cart"></span></label>
+
+    <h3 class="logo">Food Delivery Services</h3>
+
+    <!-- sidebar -->
+    <div class="sidebar">
+        <div class="sidebar-menu">
+            <span class="fas fa-search"></span>
+            <a href="#">Search</a>
+        </div>
+        <div class="sidebar-menu">
+            <span class="fas fa-home"></span>
+            <a href="homepage.php">Home</a>
+        </div>
+        <div class="sidebar-menu">
+            <span class="fas fa-heart"></span>
+            <a href="#">Favs</a>
+        </div>
+        <div class="sidebar-menu">
+            <span class="fas fa-user"></span>
+            <a href="#">Profile</a>
+        </div>
+        <div class="sidebar-menu">
+            <span class="fas fa-sliders-h"></span>
+            <a href="#">Setting</a>
+        </div>
+    </div>                          <!--                Sanya End-->
+    
+<!--main dashboard-->
+    <div class="dashboard">
+        <div class="dashboard-banner">
+            <img src="images/India’s kitchen.png">
+        </div>
+    
+        <h3 class="dashboard-title">Recommend Food For You</h3>
+<div class="dashboard-menu">
+    <a ref="#">Favorites</a>
+    <a ref="#">best Seller</a>
+    <a ref="#">Near me</a>
+    <a ref="#">promotion</a>
+    <a ref="#">Top Rated</a>
+    <a ref="#">All</a>
+</div>
+    
+
+<!--order dashboard-->
+<!--                    bhavya End-->
+
+<div class="dashboard-content">
+    <div class="dashboard-card">
+	<a href="index1.php">
+        <img class="card-image" src="images/t1.webp">
+		</a>
+        <div class="card-detail">
+            <h4>Desi Flavours Tiffin Service<span class="card-time2"></span><span class="card-time3"></span></h4>
+            <p class="card-time1"><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star-half-alt"></span>  </p>
+            <p class="card-time"><span class="fas fa-map-marker-alt"></span> 1km Away</p>
+        </div>
+    </div>
+    <div class="dashboard-card">
+	<a href="index2.php">
+        <img class="card-image" src="images/t2.jpg">
+		</a>
+        <div class="card-detail">
+            <h4>Gharwala Tiffin Service <span class="card-time2"></span><span class="card-time3"></span></h4>
+            <p class="card-time1"><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star-half-alt"></span>  </p>
+            <p class="card-time"><span class="fas fa-map-marker-alt"></span> 2kms Away</p>
+
+        </div>
+    </div>
+    <div class="dashboard-card">
+	<a href="index3.php">
+        <img class="card-image" src="images/t3.webp">
+		</a>
+        <div class="card-detail">
+            <h4>Annapurna Tiffin Service<span class="card-time2"></span></h4>
+            <p class="card-time1"><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star-half-alt"></span>  </p>
+            <p class="card-time"><span class="fas fa-map-marker-alt"></span> 3kms Away</p>
+
+        </div>
+    </div>
+    <div class="dashboard-card">
+	<a href="index4.php">
+        <img class="card-image" src="images/t4.webp">
+		</a>
+        <div class="card-detail">
+            <h4>Neha Tiffin Service<span class="card-time2"></span><span class="card-time3"></span></h4>
+            <p class="card-time1"><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star-half-alt"></span>  </p>
+            <p class="card-time"><span class="fas fa-map-marker-alt"></span> 2kms Away</p>
+
+        </div>
+    </div>
+    <div class="dashboard-card">
+	<a href="index3.php">
+        <img class="card-image" src="images/t5.jpg">
+		</a>
+        <div class="card-detail">
+            <h4>Mehar Tiffin Services<span class="card-time2"></span></h4>
+            <p class="card-time1"><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span> </p>
+            <p class="card-time"><span class="fas fa-map-marker-alt"></span> 1km Away</p>
+
+        </div>
+    </div>
+    <div class="dashboard-card">
+	<a href="index2.php">
+        <img class="card-image" src="images/t6.jpg">
+		</a>
+        <div class="card-detail">
+            <h4>Indian Tiffin Services<span class="card-time2"></span><span class="card-time3"></span></h4>
+            <p class="card-time1"><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star"></span><span class="fas fa-star-half-alt"></span>  </p>
+            <p class="card-time"><span class="fas fa-map-marker-alt"></span> 0.5km Away</p>
+
+        </div>
+    </div>
+
+
+</div>
+<!--ek div aur hai yaha-->
+</div>
+<!--line 199 44:50-->
+<div class="dashboard-order">
+
+
+<!--                            Manan End-->
+
+
+
+    <!--                    Sid end-->
+
+
+    
+
+</div>
+</body>
+</html>
