41c2cd13c37ecf60832319554b51a6f372eaf168
hereismanan <167669567+hereismanan@users.noreply.github.com>
2024-04-21 13:04:57 +0530

    Add files via upload
    Commit by Manan Sharma

---
+++ aboutus.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <title>Homepage for India's Kitchen</title>
+        <link rel="stylesheet" href="aboutus.css">
+    </head>
+    <body>
+        <div class="banner">
+            <div class="navbar">
+                <img src="42bg.png"  class="logo">
+                <ul>
+                    <li><a href="homepage.php">Home</a></li>
+                    <li><a href="register_seller.php">Register As Seller</a></li>
+                    <li><a href="login_form.php">Login</a></li>
+                </ul>
+            </div>
+        <div class="content">
+            <h1>About Us</h1>
+            <p>Welcome to India's Kitchen, where we believe that healthy eating should be affordable and convenient for everyone. Our journey began with a simple idea: to provide nutritious, balanced tiffins at an affordable price, making it easier for busy individuals and families to enjoy delicious, home-cooked meals.</p>
+            <h2>Our Mission</h2>
+            <p> At India's Kitchen, our mission is to make healthy eating accessible to all. We carefully curate our tiffin menus to include a variety of dishes that are not only delicious but also packed with essential nutrients. Whether you're looking for vegetarian, vegan, or meat options, we have something for everyone. We aim to promote local vendors, help grow their buisnesses and provide an extensive network to connect with wider customers. We aim to boost employment and encourage individuals to showcase their talents through our services. </p>
+            
+        </div>    
+
+
+        </div>
+
+    </body>
+</html>
---
+++ homepage.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <title>Homepage for India's Kitchen</title>
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    <body>
+        <div class="banner">
+            <div class="navbar">
+                <img src="42bg.png"  class="logo">
+                <ul>
+               
+                    <li><a href="aboutus.php">About Us</a></li>
+                    <li><a href="register_seller.php">Register As Seller</a></li>
+                </ul>
+            </div>
+        <div class="content">
+            <h1>GET THAT GHAR WAALA KHANNA</h1>
+            <p>with your own INDIA'S KITCHEN</p>
+            <div>
+               
+                <button type="button"  onclick="window.location.href='register_form.php'"><span></span>SIGN UP</button>
+                <button type="button"  onclick="window.location.href='login_form.php'"><span></span>LOG IN</button>
+
+            </div>
+        </div>    
+
+
+        </div>
+
+    </body>
+</html>
